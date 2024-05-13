73962f6124c2763cfb5584b6b0417041996d1289
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-14 00:28:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+        this._width = width;
+        this._height = height;
+    }
 
-class Square extends Animal {}
+    get width(){
+        return this._width;
+    }
+
+    get height(){
+        if(this._height!=null)
+            return this._height;
+        else
+            return this._width;
+    }
+
+    getArea(){
+        if(this._height!=null)
+            return (this._width * this._height);
+        else
+            return (this._width * this._width);
+    }
+}
+
+class Square extends Rectangle {
+	constructor(side){
+        super(side);
+    }
+
+    getPerimeter(){
+        return 4*this._width;
+    }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
